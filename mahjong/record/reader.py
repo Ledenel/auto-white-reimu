701d881bcbd24ab888689d7b0a0705b492d65c12
Ledenel <ledenelintelli@gmail.com>
2019-01-24 01:28:34 +0800

    add flush meld translation.

--- mahjong/record/reader.py
+++ mahjong/record/reader.py
@@ -160,8 +160,10 @@ class TenhouGame:
 """
 http://tenhou.net/0/?log=2018110323gm-0009-0000-e81b9df3&tw=1
 http://tenhou.net/0/?log=2012060420gm-0009-10011-acfd4b57
-http://tenhou.net/0/?log=2019011500gm-00a9-0000-297d5c4d
-http://tenhou.net/0/?log=2019011500gm-00b9-0000-3065ca3c
+4f:http://tenhou.net/0/?log=2019011500gm-00a9-0000-297d5c4d
+3f:http://tenhou.net/0/?log=2019011500gm-00b9-0000-3065ca3c
+
+4f-disconnected:http://tenhou.net/0/?log=2019012301gm-00a9-0000-420858f6&tw=0&tdsourcetag=s_pcqq_aiomsg
 """
 
 
--- mahjong/record/util.py
+++ mahjong/record/util.py
@@ -5,6 +5,8 @@ import bitstruct
 
 import struct
 
+from record.reader import TenhouPlayer
+
 flush_desc = """
 kui:u2
 syuntsu:b1
@@ -84,8 +86,38 @@ class Meld(metaclass=ABCMeta):
 
 
 class Flush(Meld):
-    pass
+    def __init__(self, who_index, value) -> None:
+        super().__init__()
+        self.who_index = who_index
+        data: FlushData = self.unpack(value)
+        start_flush_kind = data.type6 // 3
+        start_tile_kind = (start_flush_kind // 7) * 9 + (start_flush_kind % 7)
+        start_tile = start_tile_kind * 4
+        self._which_first = data.type6 % 3
+        base0, base1, base2 = [start_tile + 4 * i for i in range(3)]
+        self.tiles = [base0 + data.hai0, base1 + data.hai1, base2 + data.hai2]
+        self._self_tiles = [x for i, x in enumerate(self.tiles) if i != self._which_first]
+        self._borrowed_tiles = self.tiles[self._which_first]
 
+    @property
+    def self_tiles(self):
+        return self._self_tiles
+
+    @property
+    def borrowed_tiles(self):
+        return self._borrowed_tiles
+
+    @property
+    def from_who(self):
+        return (self.who_index - 1) % 4
+
+    @property
+    def data_class(self):
+        return FlushData
+
+    @property
+    def unpacker(self):
+        return flush_packer
 
 
 FlushData = named_tuple_from_desc("flush_data", flush_desc)
