816f576cae20db39b8cd3610a6b4c291845df783
canuse <44812363+canuse@users.noreply.github.com>
2019-11-22 23:59:19 +0800

    to linux path

--- tests/test_fromFile.py
+++ tests/test_fromFile.py
@@ -1,11 +1,10 @@
 from unittest import TestCase
-import os
 
 from mahjong.record.reader import from_file
 
 
 class TestFromFile(TestCase):
     def test_from_file(self):
-        filename = "tests\\2009060321gm-00b9-0000-75b25bcf.xml"
+        filename = "tests/2009060321gm-00b9-0000-75b25bcf.xml"
         record = from_file(filename)
         assert len(record.game_list) == 1
