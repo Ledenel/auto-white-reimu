11dfb8a7a469f5ae47f67b0122848886c26fccef
canuse <44812363+canuse@users.noreply.github.com>
2019-11-23 23:45:05 +0800

    fix duplicate akatile and tile in tile_hash

--- mahjong/tile/definition.py
+++ mahjong/tile/definition.py
@@ -120,22 +120,19 @@ def tile_flush(tile: Tile) -> TileSet:
 class AkaTile(Tile):
     def __new__(cls, number: int, color: str):
         tile_hash = tile_hash_num(color, number + 5)
-        if tile_hash in Tile._TILE_POOL:
-            return Tile._TILE_POOL[tile_hash]
-        else:
-            tile_obj = object.__new__(cls)
-            self = tile_obj
-            self._hash = tile_hash
-            self._number = number
-            self._color = color
-            self._tuple_view = (color, number + 5)
-            Tile._TILE_POOL[tile_hash] = tile_obj
-            self._flush = TileSet(
-                (Tile(self._number + 5 + i, self._color) for i in range(Tile.FLUSH_LENGTH))
-            ) if self.is_suit() and self._number == 0 else None
-            self._triplet = self.repeat(Tile.TRIPLET_LENGTH)
-            self._pair = self.repeat(Tile.PAIR_LENGTH)
-            return tile_obj
+        tile_obj = object.__new__(cls)
+        self = tile_obj
+        self._hash = tile_hash
+        self._number = number
+        self._color = color
+        self._tuple_view = (color, number + 5)
+        Tile._TILE_POOL[tile_hash] = tile_obj
+        self._flush = TileSet(
+            (Tile(self._number + 5 + i, self._color) for i in range(Tile.FLUSH_LENGTH))
+        ) if self.is_suit() and self._number == 0 else None
+        self._triplet = self.repeat(Tile.TRIPLET_LENGTH)
+        self._pair = self.repeat(Tile.PAIR_LENGTH)
+        return tile_obj
 
     def next(self) -> Tile:
         if self.is_suit() and self._number < Tile.SUIT_MAX:
