5cf13f609d6f25939ffda5ca785e039dec565d7b
canuse <44812363+canuse@users.noreply.github.com>
2019-11-26 21:34:02 +0800

    change return type

--- mahjong/record/utils/event.py
+++ mahjong/record/utils/event.py
@@ -118,29 +118,34 @@ class TenhouEvent:
         event = self._wrapped_xml_event
         attrs = event.attrib
         if event.tag == 'INIT':
-            dict0 = {'event_type': 'INIT', 'player': '0', 'player_see': attrs['hai0'],
-                     'special': attrs['ten'].split(',')[0]}
-            dict1 = {'event_type': 'INIT', 'player': '1', 'player_see': attrs['hai1'],
-                     'special': attrs['ten'].split(',')[1]}
-            dict2 = {'event_type': 'INIT', 'player': '2', 'player_see': attrs['hai2'],
-                     'special': attrs['ten'].split(',')[2]}
-            dora = {'event_type': 'DORA', 'player': '0', 'player_see': attrs['seed'].split(',')[-1],
-                    'player_show': attrs['seed'].split(',')[-1]}
-            initround = {'event_type': 'ROUN', 'special': attrs['seed'].split(',')[2]}
+            dict0 = {'event_type': 'INIT1', 'player': '0', 'player_see': ''.join(
+                [str(tile_from_tenhou(i)) for i in sorted([int(i) for i in attrs['hai0'].split(',')])]),
+                     'score': attrs['ten'].split(',')[0] + '00'}
+            dict1 = {'event_type': 'INIT2', 'player': '1', 'player_see': ''.join(
+                [str(tile_from_tenhou(i)) for i in sorted([int(i) for i in attrs['hai1'].split(',')])]),
+                     'score': attrs['ten'].split(',')[1] + '00'}
+            dict2 = {'event_type': 'INIT3', 'player': '2', 'player_see': ''.join(
+                [str(tile_from_tenhou(i)) for i in sorted([int(i) for i in attrs['hai2'].split(',')])]),
+                     'score': attrs['ten'].split(',')[2] + '00'}
+            dora = {'event_type': 'DORA ', 'player': '0',
+                    'player_see': str(tile_from_tenhou(int(attrs['seed'].split(',')[-1]))),
+                    'player_show': str(tile_from_tenhou(int(attrs['seed'].split(',')[-1])))}
+            initround = {'event_type': 'ROUND', 'score': attrs['seed'].split(',')[2] + '000'}
             if 'hai3' in attrs.keys():
-                dict3 = {'event_type': 'INIT', 'player': '3', 'tiles': attrs['hai3'],
-                         'special': attrs['ten'].split(',')[3]}
-                return dict0, dict1, dict2, dict3, dora, initround
+                dict3 = {'event_type': 'INIT4', 'player': '3', 'player_see': ''.join(
+                    [str(tile_from_tenhou(i)) for i in sorted([int(i) for i in attrs['hai3'].split(',')])]),
+                         'score': attrs['ten'].split(',')[3] + '00'}
+                return [dict0, dict1, dict2, dict3, dora, initround]
             else:
-                return dict0, dict1, dict2, dora, initround
+                return [dict0, dict1, dict2, dora, initround]
         draw = draw_tile_change(event)
         if draw:
             player, tile = draw['player'], draw['tile']
-            return {'event_type': 'DRAW', 'player': player, 'player_see': tile}
+            return [{'event_type': 'DRAW ', 'player': player, 'player_see': str(tile)}]
         discard = discard_tile_change(event)
         if discard:
-            player, tile = draw['player'], draw['tile']
-            return {'event_type': 'DISC', 'player': player, 'player_show': tile}
+            player, tile = discard['player'], discard['tile']
+            return [{'event_type': 'DISCA', 'player': player, 'player_show': str(tile)}]
         if is_open_hand(event):
 
             player = attrs['who']
@@ -150,44 +155,57 @@ class TenhouEvent:
             self_tile = item.self_tiles
             borrow_tile = item.borrowed_tiles
             if type_of.syuntsu:
-                return {'event_type': 'CHI ', 'player': player, 'player_show': str(self_tile),
-                        'player_open': str(borrow_tile) + str(self_tile), 'special': source}
+                return [{'event_type': 'CHI  ', 'player': player,
+                         'player_show': ''.join([str(tile_from_tenhou(int(i))) for i in self_tile]),
+                         'player_open': ''.join([str(tile_from_tenhou(int(i))) for i in borrow_tile]) + ''.join(
+                             [str(tile_from_tenhou(int(i))) for i in self_tile]), 'origin': source}]
             elif type_of.koutsu:
-                return {'event_type': 'PENG', 'player': player, 'player_show': str(self_tile),
-                        'player_open': str(borrow_tile) + str(self_tile), 'special': source}
+                return [{'event_type': 'PENG ', 'player': player,
+                         'player_show': ''.join([str(tile_from_tenhou(int(i))) for i in self_tile]),
+                         'player_open': ''.join([str(tile_from_tenhou(int(i))) for i in borrow_tile]) + ''.join(
+                             [str(tile_from_tenhou(int(i))) for i in self_tile]), 'origin': source}]
             elif type_of.chakan:
-                return {'event_type': 'GANG', 'player': player, 'player_show': str(self_tile),
-                        'player_open': str(borrow_tile) + str(self_tile), 'special': source}
+                return [{'event_type': 'GANG', 'player': player,
+                         'player_show': ''.join([str(tile_from_tenhou(int(i))) for i in self_tile]),
+                         'player_open': ''.join([str(tile_from_tenhou(int(i))) for i in borrow_tile]) + ''.join(
+                             [str(tile_from_tenhou(int(i))) for i in self_tile]), 'origin': source}]
             elif type_of.nuki:
-                return {'event_type': 'KITA', 'player': player, 'player_show': str(self_tile),
-                        'player_open': str(self_tile)}
+                return [{'event_type': 'KITA ', 'player': player,
+                         'player_show': str([tile_from_tenhou(i) for i in self_tile][0]),
+                         'player_open': str([tile_from_tenhou(i) for i in self_tile][0])}]
             else:
                 if len(borrow_tile) == 0:
-                    return {'event_type': 'AGAN', 'player': player, 'player_show': str(self_tile),
-                            'player_open': str(self_tile), 'special': source}
+                    return [{'event_type': 'ANGAN', 'player': player,
+                             'player_show': ''.join([str(tile_from_tenhou(int(i))) for i in self_tile]),
+                             'player_open': ''.join([str(tile_from_tenhou(int(i))) for i in self_tile]),
+                             'origin': source}]
                 else:
-                    return {'event_type': 'MGAN', 'player': player, 'player_show': str(self_tile),
-                            'player_open': str(borrow_tile) + str(self_tile), 'special': source}
+                    return [{'event_type': 'MNGAN', 'player': player,
+                             'player_show': ''.join([str(tile_from_tenhou(int(i))) for i in self_tile]),
+                             'player_open': ''.join([str(tile_from_tenhou(int(i))) for i in borrow_tile]) + ''.join(
+                                 [str(tile_from_tenhou(int(i))) for i in self_tile]), 'origin': source}]
 
         if is_richii(event):
             player = attrs['who']
             status = attrs['step']
             if status == '1':
-                return {'event_type': 'RICH', 'player': player, 'special': 'CALL RICHI'}
+                return [{'event_type': 'CALRC', 'player': player}]
             if status == '2':
-                richi = {'event_type': 'RICH', 'player': player, 'special': 'RICHI SUCCESS'}
-                score = {'event_type': 'SCOR', 'player': player, 'special': '-1000'}
-                return richi, score
+                richi = {'event_type': 'RICHI', 'player': player}
+                score = {'event_type': 'SCORE', 'player': player, 'score': '-1000'}
+                return [richi, score]
 
         if is_dora_indicator_event(event):
             tile = tile_from_tenhou(int(attrs['hai']))
-            return {'event_type': 'DORA', 'player': '0', 'player_see': tile, 'player_show': tile}
+            return [{'event_type': 'DORA ', 'player': '0', 'player_see': tile, 'player_show': tile}]
 
         if is_somebody_win_game(event):
             player = attrs['who']
             loser = attrs['fromWho']
             score = number_list(event.attrib['ten'])[1]
-            return {'event_type': 'WIN ', 'player': player, 'special': loser}
+            return [{'event_type': 'WIN  ', 'player': player, 'origin': loser}]
 
         if is_nobody_win_game(event):
-            return {'event_type': 'EVEN', 'special': DRAWN_TYPES[attrs['type']]}
+            return [{'event_type': 'EVEN ', 'origin': DRAWN_TYPES[attrs['type']] if 'type' in attrs else ''}]
+
+        return ['UNPARSED']
