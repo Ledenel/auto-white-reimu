f88742a3ff1c3f47f24e626ea84ded96b2f635bd
canuse <44812363+canuse@users.noreply.github.com>
2019-11-27 15:09:50 +0800

    add test for fin_game

--- tests/test_paifu.py
+++ tests/test_paifu.py
@@ -15,8 +15,6 @@ class TestTile(TestCase):
                 retlen += len(ret)
         assert retlen == 50
 
-        assert len(a.to_paifu()) == retlen
-
     def test_init_to_paifu_sanma(self):
         a = from_file("tests/2009060321gm-00b9-0000-75b25bcf.xml")
         assert len(a.game_list[0].events[0].to_paifu()) == 5
@@ -28,8 +26,23 @@ class TestTile(TestCase):
     def test_pd(self):
         a = from_file("tests/2009060321gm-00b9-0000-75b25bcf.xml")
         pd_frame = pd.DataFrame(a.to_paifu())
-        assert len(pd_frame) == 50
+        assert len(pd_frame) == 53
 
         a = from_file("tests/2009100718gm-00e1-0000-470654d3.xml")
         pd_frame = pd.DataFrame(a.to_paifu())
-        assert len(pd_frame) == 71
+        assert len(pd_frame) == 75
+
+    def test_fin_game(self):
+        a = from_file("tests/2009060321gm-00b9-0000-75b25bcf.xml")
+        tr = len(a.to_paifu())
+        tg = 0
+        for i in a.game_list:
+            tg += len(i.to_paifu())
+        assert tr - tg == 3
+
+        a = from_file("tests/2009100718gm-00e1-0000-470654d3.xml")
+        tr = len(a.to_paifu())
+        tg = 0
+        for i in a.game_list:
+            tg += len(i.to_paifu())
+        assert tr - tg == 4
