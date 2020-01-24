4599bdc3c501d3dcf67ff6e22602433e463a18d6
Ledenel <ledenelintelli@gmail.com>
2020-01-24 16:40:23 +0800

    add macos and create release only on create.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto_white_reimu",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     url="",
     license="GPL",
