ee3e3e065c0b7a43ba889f6464f4a412c455a8e3
Ledenel <ledenelintelli@gmail.com>
2020-01-26 20:13:07 +0800

    sorting tests.

--- tests/uni_record/test_event_definition.py
+++ tests/uni_record/test_event_definition.py
@@ -3,6 +3,15 @@ from mahjong.record.universe.tenhou import to_commands
 
 test_file = "tests/2009060321gm-00b9-0000-75b25bcf.xml"
 
+command_list = None
+
+def command_setup():
+    global command_list
+    with open(test_file, "r") as f:
+        record = from_file(f)
+    command_list = to_commands(record)
+
+command_setup()
 
 def test_command_convert():
     with open(test_file, "r") as f:
