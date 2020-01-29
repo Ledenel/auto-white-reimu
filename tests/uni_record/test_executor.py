3f2168f9fb8c405d9625ed16cc7dd4dcb6a551b1
Ledenel <ledenelintelli@gmail.com>
2020-01-29 16:42:34 +0800

    add paifu auto-grab

--- tests/uni_record/test_executor.py
+++ tests/uni_record/test_executor.py
@@ -1,34 +1,37 @@
 import pandas as pd
+import pytest
 
 from mahjong.record.universe.executor import GameExecutor, replace_enum_values
-from tests.uni_record.test_event_definition import get_test_commands
+from tests.uni_record.paifu_list import test_files
+from tests.uni_record.test_event_definition import to_commands_iter
 
-commands = get_test_commands()
 
-
-def test_execute():
+@pytest.mark.parametrize("commands", to_commands_iter(test_files), ids=test_files)
+def test_execute(commands):
     assert len(list(GameExecutor().execute(commands))) > 1
 
 
-# TODO: add export execute to csv (flatten for TransferDict).
-
-def test_execute_export():
+@pytest.mark.parametrize("commands", to_commands_iter(test_files), ids=test_files)
+def test_execute_export(commands):
     df = pd.DataFrame(x.flatten() for x in GameExecutor().execute(commands))
     df.columns = pd.MultiIndex.from_tuples(df.columns)
     df.to_csv("state_tenhou.csv")
     assert len(df) > 1
 
 
-def test_execute_enum_transform():
+@pytest.mark.parametrize("commands", to_commands_iter(test_files), ids=test_files)
+def test_execute_enum_transform(commands):
     df = pd.DataFrame(x.flatten() for x in GameExecutor().execute(commands))
     old_columns = df.columns
     df.columns = pd.MultiIndex.from_tuples(df.columns)
     df.to_csv("state_tenhou_2.csv")
-    df_new = pd.read_csv("state_tenhou_2.csv", header=[0,1,2], skipinitialspace=True)
+    df_new = pd.read_csv("state_tenhou_2.csv", header=[0, 1, 2], skipinitialspace=True)
     df_rename = df_new.rename(columns=replace_enum_values)
     assert set(old_columns).issubset(set(df_rename.columns))
 
-def test_read_clean_csv():
+
+@pytest.mark.parametrize("commands", to_commands_iter(test_files), ids=test_files)
+def test_read_clean_csv(commands):
     df = pd.DataFrame(x.flatten() for x in GameExecutor().execute(commands))
     df.columns = pd.MultiIndex.from_tuples(df.columns)
     df.to_csv("state_tenhou_3.csv")
