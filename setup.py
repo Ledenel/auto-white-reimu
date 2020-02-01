f1f1800a3acb4fa15bfce3a287aedbf0a6225851
Ledenel <ledenelintelli@gmail.com>
2020-02-01 22:07:06 +0800

    increase version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto_white_reimu",
-    version="0.1.4",
+    version="0.2.0",
     packages=find_packages(),
     url="",
     license="GPL",
