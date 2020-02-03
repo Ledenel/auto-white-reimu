588ae5813e5084fbc2e23279f511b53ce1c3997c
Ledenel <ledenelintelli@gmail.com>
2020-02-03 17:28:12 +0800

    add dora and initial tiles util.

--- mahjong/record/__init__.py
+++ mahjong/record/__init__.py
@@ -1,5 +1,5 @@
-from .util_functions\
-    import parse_dataframe_from_tenhou_record,\
+from .util_functions \
+    import parse_dataframe_from_tenhou_record, \
     light_dataframe_from_file
 from .reader \
     import from_url as tenhou_from_url, \
@@ -7,3 +7,6 @@ from .reader \
     log_id_from_url as tenhou_log_id_from_url, \
     log_id_to_url as tenhou_log_id_to_url, \
     download_url as tenhou_download_url
+
+from .const import doras, initial_tiles
+
