f2f0f652e6a9c0324737c1ed9b047bc0cf2b0d97
Ledenel <ledenelintelli@gmail.com>
2020-02-04 13:42:22 +0800

    increase setup.py speed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto_white_reimu",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     url="",
     license="GPL",
